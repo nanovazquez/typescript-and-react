2370f50b986c537e433c934d1052638235f7ec8d
nanovazquez <marianodvazquez@gmail.com>
2018-11-08 11:26:07 -0500

    4. Send props to TopBar and SidePanel

--- end/runtime-manager/src/App.tsx
+++ end/runtime-manager/src/App.tsx
@@ -2,12 +2,17 @@ import React, { Component } from "react";
 import { ApplicationsPage, SidePanel, TopBar } from "./components";
 import styles from "./App.module.css";
 
+const productName = "Runtime Manager";
+const user = { id: "1", name: "Nano Vazquez", initials: "MV" };
+const pages = [{ url: "1", title: "Applications" }, { url: "2", title: "Servers" }, { url: "3", title: "Alerts" }];
+const activePage = pages[0];
+
 class App extends Component {
   render() {
     return (
       <div className={styles.app}>
-        <TopBar />
-        <SidePanel />
+        <TopBar user={user} productName={productName} />
+        <SidePanel pages={pages} activePage={activePage} />
         <ApplicationsPage />
       </div>
     );

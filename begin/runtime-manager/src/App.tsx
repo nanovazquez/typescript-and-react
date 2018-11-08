c0b0b362ffc928d4bde5111db2d37c3aa080071d
nanovazquez <marianodvazquez@gmail.com>
2018-11-08 11:24:53 -0500

    Begin/End: Initial setup

--- begin/runtime-manager/src/App.tsx
+++ begin/runtime-manager/src/App.tsx
@@ -2,8 +2,6 @@ import React, { Component } from "react";
 import logo from "./logo.svg";
 import styles from "./App.module.css";
 
-console.log("hi", styles);
-
 class App extends Component {
   render() {
     return (
---
+++ end/runtime-manager/src/components/types.ts
@@ -0,0 +1,10 @@
+export interface IUser {
+  id: string;
+  name: string;
+  initials: string;
+}
+
+export interface IPage {
+  url: string;
+  title: string;
+}
